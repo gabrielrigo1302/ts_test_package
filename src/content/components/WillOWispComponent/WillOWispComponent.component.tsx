acd7c6356fb84e8cb8773a52148d5f080807cdef
Gabriel <gabrieloliveirarigo13@gmail.com>
2025-01-06 23:36:15 -0300

    test: generation test

---
+++ src/content/components/WillOWispComponent/WillOWispComponent.component.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { IWillOWispComponentProps } from '../../../__types__/components/WillOWispComponent';
+
+export const WillOWispComponent = (props: IWillOWispComponentProps) => {
+  return (<></>);
+}
