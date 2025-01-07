93f3d46dc762efbf791653ec135b160c73151ed4
Gabriel <gabrieloliveirarigo13@gmail.com>
2025-01-07 16:53:36 -0300

    wip: plop templates

--- plopfile.js
+++ plopfile.js
@@ -1,37 +1,73 @@
 module.exports = function (plop) {
-    plop.setGenerator('component', {
-        description: 'React component using Typescript',
-        prompts: [
-          {
-            type: 'input',
-            name: 'name',
-            message: 'Name: '
-          },
-        ],
-        actions: [
-          {
-            type: 'addMany',
-            destination: 'src/content/components/{{name}}',
-            templateFiles: 'src/templates/components/*.hbs',
-            base: 'src/templates/components',
-          },
-          {
-            type: 'modify',
-            path: 'src/content/components/index.ts',
-            pattern: /$/,
-            template: `\nexport * from './{{name}}'`
-          },
-          {
-            type: 'add',
-            path: 'src/__types__/components/{{name}}/index.ts',
-            templateFile: 'src/templates/__types__/components/index.ts.hbs',
-          },
-          {
-            type: 'modify',
-            path: 'src/__types__/components/index.ts',
-            pattern: /$/,
-            template: `\nexport * from './{{name}}'`
-          }
-        ]
-    });
+  plop.setGenerator('component', {
+    description: 'Create React Component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name: '
+      },
+    ],
+    actions: [
+      {
+        type: 'addMany',
+        destination: 'src/content/components/{{name}}',
+        templateFiles: 'src/templates/components/*.hbs',
+        base: 'src/templates/components',
+      },
+      {
+        type: 'modify',
+        path: 'src/content/components/index.ts',
+        pattern: /$/,
+        template: `\nexport * from './{{name}}'`
+      },
+      {
+        type: 'add',
+        path: 'src/__types__/components/{{name}}/index.ts',
+        templateFile: 'src/templates/__types__/components/index.ts.hbs',
+      },
+      {
+        type: 'modify',
+        path: 'src/__types__/components/index.ts',
+        pattern: /$/,
+        template: `\nexport * from './{{name}}'`
+      }
+    ]
+  });
+
+  plop.setGenerator('component', {
+    description: 'Create React Component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name: '
+      },
+    ],
+    actions: [
+      {
+        type: 'addMany',
+        destination: 'src/content/components/{{name}}',
+        templateFiles: 'src/templates/components/*.hbs',
+        base: 'src/templates/components',
+      },
+      {
+        type: 'modify',
+        path: 'src/content/components/index.ts',
+        pattern: /$/,
+        template: `\nexport * from './{{name}}'`
+      },
+      {
+        type: 'add',
+        path: 'src/__types__/components/{{name}}/index.ts',
+        templateFile: 'src/templates/__types__/components/index.ts.hbs',
+      },
+      {
+        type: 'modify',
+        path: 'src/__types__/components/index.ts',
+        pattern: /$/,
+        template: `\nexport * from './{{name}}'`
+      }
+    ]
+  });
   };
